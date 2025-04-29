37b27fe8ad3f70352d4143f5c3de53e38475ffc7
Shuflduf <shuflduf@shuflduf.xyz>
2025-04-29 14:57:46 -0600

    sdkasdlaksdmas

--- hacklet.js
+++ hacklet.js
@@ -1,18 +1,38 @@
 javascript: (async function () {
   const inQuestionText = document.querySelector(
-    '[class*="question-title__Title-"]',
+    '[class*="question-title__Title-"]'
   );
   const inPreQuestionText = document.querySelector(
-    '[class*="block-title__Title-"]',
+    '[class*="block-title__Title-"]'
   );
-  const text = inQuestionText
+  let text = inQuestionText
     ? inQuestionText.textContent
     : inPreQuestionText
       ? inPreQuestionText.textContent
       : null;
+
+  let answers = [];
+  document.querySelectorAll(
+    '[data-functional-selector*="question-choice-text-"]',
+  ).forEach((item) => {
+    answers.push(item.children[0].textContent);
+  });
+
+  if (answers.length !== 0) {
+    const answer = answers.join("\n");
+    text += "\n"
+    text += "ANSWERS:\n"
+    text += answer;
+  }
+
   const GEMINI_API_KEY = localStorage.getItem("GEMINI_API_KEY");
   if (!GEMINI_API_KEY) {
-    localStorage.setItem("GEMINI_API_KEY", prompt("Enter your Gemini API key. You can get an API key from https://aistudio.google.com/apikey: "));
+    localStorage.setItem(
+      "GEMINI_API_KEY",
+      prompt(
+        "Enter your Gemini API key. You can get an API key from https://aistudio.google.com/apikey: ",
+      ),
+    );
     return;
   }
   const url =
@@ -27,9 +47,9 @@ javascript: (async function () {
       system_instruction: {
         parts: [
           {
-            "text": "You are a Kahoot AI. You will be given the text of a question and you will have to answer it correctly. Keep your answer short and concise. Do not add any extra information.",
-          }
-        ]
+            text: "You are a Kahoot AI. You will be given the text of a question and you will have to answer it correctly. Keep your answer short and concise. Do not add any extra information.",
+          },
+        ],
       },
       generationConfig: {
         temperature: 0.0,
